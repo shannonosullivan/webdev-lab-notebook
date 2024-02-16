9981184d4e093d3efc13e2bd521d30589087f183
shannonosullivan <shanhulud@gmail.com>
2024-02-16 14:12:52 -0800

    finished 02

--- 07-javascript/02-format.js
+++ 07-javascript/02-format.js
@@ -4,6 +4,67 @@ const formatItems = (input) => {
   // for reference, see the MDN Number page:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
 
+  let numString = "";
+  const arrayLength = input.length;
+
+  for (let i = 0; i < arrayLength; ++i){
+    const num = String(input[i]);
+    const decimalIndex = num.indexOf('.');
+    //number already in floating point form
+    if (decimalIndex !== -1){
+      const numLength = num.length;
+      const decimalToLastIndex = numLength - 1 - decimalIndex;
+      //more than 3 digits after decimal place in original number
+      if (decimalToLastIndex > 3){
+        const lastIndex = decimalIndex + 4;
+        const finalNumber = num.substring(0, lastIndex);
+        if (i === 0){
+          numString += finalNumber;
+        }
+        else{
+          numString += ", " + finalNumber;
+        }
+      }
+      //exactly 3 digits after the decimal point in original number
+      else if (decimalToLastIndex === 3){
+        if (i === 0){
+          numString += num;
+        }
+        else{
+          numString += ", " + num;
+        }
+      }
+      //exactly 2 digits after the decimal point in original number
+      else if (decimalToLastIndex === 2){
+        if (i === 0){
+          numString += num + "0";
+        }
+        else{
+          numString += ", " + num + "0";
+        }
+      }
+      //exactly 1 digit after the decimal point in original number
+      else if (decimalToLastIndex === 1){
+        if (i === 0){
+          numString += num + "00";
+        }
+        else{
+          numString += ", " + num + "00";
+        }
+      }
+    }
+    //integer
+    else{
+      if (i === 0){
+        numString += num + ".000";
+      }
+      else{
+        numString += ", " + num + ".000";
+      }
+    }
+  }
+
+  input = numString;
   return input;
 };
 

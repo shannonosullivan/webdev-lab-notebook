592e4371350de86123ce3b150bda188c7b013699
shannonosullivan <shanhulud@gmail.com>
2024-02-16 13:50:05 -0800

    finished 01

--- 07-javascript/01-replace.js
+++ 07-javascript/01-replace.js
@@ -1,8 +1,16 @@
 const replaceItems = (input) => {
   // return a string with 'gold' replaced by 'SHINY' and 'wander' replaced by 'roam', regardless of the case of the letters
 
-  // for reference, see the MDN "Regular Expressions" entry:
+  // for reference, see the MDN "Regular Expressions" and "matchAll()" entries:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replaceAll
+  const goldRegex = /gold/gi;
+  const wanderRegex = /wander/gi;
+  const shiny = "SHINY";
+  const roam = "roam";
+
+  input = input.replaceAll(goldRegex, shiny);
+  input = input.replaceAll(wanderRegex, roam);
 
   return input;
 };
